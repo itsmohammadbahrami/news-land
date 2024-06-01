6965614dd529d8f08a86a47fde2b97437385a8ac
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-01 15:50:58 +0330

    feat: config redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import StoreProvider from "./StoreProvider";
 
 import "./globals.css";
 
@@ -19,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>
-          {children}
-        </AntdRegistry>
+        <StoreProvider>
+          <AntdRegistry>
+            {children}
+          </AntdRegistry>
+        </StoreProvider>
       </body>
     </html>
   );

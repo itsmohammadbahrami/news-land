0ad9d1f3cbe9c6703a824a0770ce0e6c1f285867
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-03 12:28:46 +0330

    feat: update api configs

---
+++ services/apis.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const createAxios = (baseURL?: string) =>
+    axios.create({
+        baseURL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+const [axiosNewsAPI, axiosGuardianAPI, axiosNYTimesAPI] = [
+    createAxios(process.env.NEXT_PUBLIC_NEWS_API_BASE_URL),
+    createAxios(process.env.NEXT_PUBLIC_GUARDIAN_API_BASE_URL),
+    createAxios(process.env.NEXT_PUBLIC_NYTIMES_API_BASE_URL),
+];
+
+const axiosInstances = {
+    axiosNewsAPI,
+    axiosGuardianAPI,
+    axiosNYTimesAPI
+};
+
+const axiosAPIKeys = {
+    axiosNewsAPI: {
+        urlKey: 'apiKey',
+        apiKey: process.env.NEXT_PUBLIC_NEWS_API_KEY,
+    },
+    axiosGuardianAPI: {
+        urlKey: 'api-key',
+        apiKey: process.env.NEXT_PUBLIC_GUARDIAN_API_KEY,
+    },
+    axiosNYTimesAPI: {
+        urlKey: 'api-key',
+        apiKey: process.env.NEXT_PUBLIC_NYTIMES_API_KEY,
+    },
+};
+
+Object.keys(axiosInstances).forEach(axiosKey => {
+    const key = axiosKey as keyof typeof axiosInstances;
+    axiosInstances[key].interceptors.request.use(
+        request => {
+            request.params = {
+                ...request.params,
+                [axiosAPIKeys[key].urlKey]:
+                    axiosAPIKeys[key].apiKey,
+            };
+            return request;
+        },
+        error => {
+            return Promise.reject(error);
+        }
+    );
+});
+
+export { axiosNewsAPI, axiosGuardianAPI, axiosNYTimesAPI };

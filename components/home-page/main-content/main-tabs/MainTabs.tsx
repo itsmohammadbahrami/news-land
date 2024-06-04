6f97b80f735193399861751b0de899c9bea223ed
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-04 16:37:46 +0330

    feat(main content): get data on tab change

--- components/home-page/main-content/main-tabs/MainTabs.tsx
+++ components/home-page/main-content/main-tabs/MainTabs.tsx
@@ -1,22 +1,25 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
+import { useAppDispatch } from '@/store/hooks'
 import { Tabs, TabsProps } from 'antd'
 import { getArray } from '@/utils/utils'
+import { getNews } from '@/store/slices/news/news.api'
 
 
 const MainTabs = () => {
     const texts = useTranslations('mainContent')
+    const dispatch = useAppDispatch()
 
     const tabItems: TabsProps['items'] =
         getArray(texts('tabs')).map(tab => ({
             key: tab,
             label: tab,
-            children: tab
+            children: ''
         }))
 
     return (
-        <Tabs defaultActiveKey="1" items={tabItems} onChange={(key) => { }} />
+        <Tabs defaultActiveKey="1" items={tabItems} onChange={(key) => { dispatch(getNews()) }} />
     )
 }
 

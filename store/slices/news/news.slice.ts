41da94cc45a725f47dc282d3bf52439d96269234
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-04 14:44:39 +0330

    feat(news slice): add extra reducers

--- store/slices/news/news.slice.ts
+++ store/slices/news/news.slice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 import { INewsState } from '@/types/news/news.type'
+import { getNews } from './news.api'
 
 const initialState: INewsState = {
     loading: false,
@@ -13,6 +14,18 @@ const newsSlice = createSlice({
     reducers: {
 
     },
+    extraReducers: (builder) => {
+        builder.addCase(getNews.pending, (state) => {
+            state.loading = true;
+        })
+            .addCase(getNews.fulfilled, (state, action) => {
+                state.loading = false;
+                state.news = action.payload;
+            })
+            .addCase(getNews.rejected, (state) => {
+                state.loading = false;
+            })
+    }
 })
 
 export const { } = newsSlice.actions

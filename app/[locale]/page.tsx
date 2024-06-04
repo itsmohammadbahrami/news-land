6d40e6ab3d9443ae28d9579445536269a0618a44
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-04 12:49:48 +0330

    feat: add home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,8 @@
+import HomePage from "@/components/home-page/HomePage";
 
 export default function Home() {
 
   return (
-    <main>
-    </main>
+    <HomePage />
   );
 }
---
+++ components/home-page/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <section className="w-full h-[calc(100vh-4.063rem)] py-4 px-[3.125rem] flex">
+            <main className='flex-1'>
+                main
+            </main>
+            <aside className='w-[20rem]'>
+                aside
+            </aside>
+        </section>
+    )
+}
+
+export default HomePage
